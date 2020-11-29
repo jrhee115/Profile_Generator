6cd68118aeadfa431357994df449b0ef1ff1e0cb
J Rhee <jrhee@Justins-MacBook-Pro.local>
2020-11-28 22:48:37 -0500

    added more given info from Joes video, then added my own info for addIntern and addEngineer

--- Develop/app.js
+++ Develop/app.js
@@ -10,26 +10,132 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const teamMember = []
+const idArray = []
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+function appMenu(){
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+    function createManager(){
+        console.log("PLease build your team");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "managerName",
+                message: "What is your name?"
+            },
+            {
+                type: "input",
+                name: "managerID",
+                message: "What is your ID number?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is your email?"
+            },
+            {
+                type: "input",
+                name: "managerOfficeNumber",
+                message: "What is your office number?"
+            },
+        ]).then(answers => {
+            const manager = new Manager(answers.managerName, answers.managerID, answers.managerEmail, answers.managerOfficeNumber);
+            teamMember.push(manager);
+            idArray.push(answers.managerID);
+            createTeam();
+        })
+    }
+    function createTeam(){
+        inquirer.prompt([
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Which type of team member would you like to add?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "I don't want to add any more team members"
+                ]
+            }
+        ]).then(userChoice => {
+            switch (userChoice.memberChoice) {
+                case "Engineer":
+                    addEngineer();
+                    break;
+                case "Intern":
+                    addIntern();
+                    break;
+                default:
+                    buildTeam();
+            }
+        });
+    }
+    function addEngineer(){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "engineerName",
+                message: "What is your name?"
+            },
+            {
+                type: "input",
+                name: "engineerID",
+                message: "What is your ID number?"
+            },
+            {
+                type: "input",
+                name: "engineerEmail",
+                message: "What is your email?"
+            },
+            {
+                type: "input",
+                name: "engineerGithub",
+                message: "What is your Github username?"
+            },
+        ]).then(answers => {
+            const engineer = new Engineer(answers.engineerName, answers.engineerID, answers.engineerEmail, answers.engineerGithub);
+            teamMember.push(engineer);
+            idArray.push(answers.engineerID);
+            createTeam();
+        })
+    }
+    function addIntern(){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "internName",
+                message: "What is your name?"
+            },
+            {
+                type: "input",
+                name: "internID",
+                message: "What is your ID number?"
+            },
+            {
+                type: "input",
+                name: "internEmail",
+                message: "What is your email?"
+            },
+            {
+                type: "input",
+                name: "internSchool",
+                message: "What school did you attend? "
+            },
+        ]).then(answers => {
+            const intern = new Intern(answers.internName, answers.internID, answers.internEmail, answers.internSchool);
+            teamMember.push(intern);
+            idArray.push(answers.internID);
+            createTeam();
+        })
+    }
+    function buildTeam(){
+        if (!fs.existsSync(OUTPUT_DIR)) {
+            fs.mkdirSync(OUTPUT_DIR)
+        }
+        fs.writeFileSync(outputPath, render(teamMember), "utf-8");
+    }
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+    createManager();
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+appMenu();
